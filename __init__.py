a834560ca86bac67d2a453c3011f2dd1e8b91698
silveroxides <ishimarukaito@gmail.com>
2026-07-01 16:55:24 +0200

    Add saving of image embeds and unfortunate monkey patch of core ComfyUI

--- __init__.py
+++ __init__.py
@@ -1,5 +1,56 @@
 from typing_extensions import override
 
+# Monkey-patch ComfyUI core's Qwen3VLTokenizer.tokenize_with_weights on startup
+# to fix a core bug where loaded embedding Tensors in the token list cause a
+# "Boolean value of Tensor with more than one value is ambiguous" crash on line 170.
+try:
+    import comfy.text_encoders.qwen3vl
+    import torch
+
+    def safe_tokenize_with_weights(self, text, return_word_ids=False, llama_template=None, images=[], prevent_empty_text=False, thinking=False, **kwargs):
+        image = kwargs.get("image", None)
+        if image is not None and len(images) == 0:
+            images = [image[i:i + 1] for i in range(image.shape[0])]
+
+        skip_template = text.startswith('<|im_start|>')
+        if prevent_empty_text and text == '':
+            text = ' '
+
+        if skip_template:
+            llama_text = text
+        else:
+            if llama_template is not None:
+                template = llama_template
+            elif len(images) == 0:
+                template = self.llama_template
+            else:
+                template = self.llama_template_images
+                if len(images) > 1:
+                    vision_block = "<|vision_start|><|image_pad|><|vision_end|>"
+                    template = template.replace(vision_block, vision_block * len(images), 1)
+            llama_text = template.format(text)
+            if not thinking:
+                llama_text += "<think>\n\n</think>\n\n"
+
+        tokens = super(comfy.text_encoders.qwen3vl.Qwen3VLTokenizer, self).tokenize_with_weights(
+            llama_text, return_word_ids=return_word_ids, disable_weights=True, **kwargs
+        )
+        key_name = next(iter(tokens))
+        embed_count = 0
+        for r in tokens[key_name]:
+            for i in range(len(r)):
+                # Core Bug Fix: Safely check type of r[i][0] before comparing to integer 151655
+                if isinstance(r[i][0], (int, float)) and r[i][0] == 151655:  # <|image_pad|>
+                    if len(images) > embed_count:
+                        r[i] = ({"type": "image", "data": images[embed_count], "original_type": "image"},) + r[i][1:]
+                        embed_count += 1
+        return tokens
+
+    comfy.text_encoders.qwen3vl.Qwen3VLTokenizer.tokenize_with_weights = safe_tokenize_with_weights
+    print("[ComfyUI-UtilsCollection] Successfully monkey-patched Qwen3VLTokenizer core tokenizer with safe embedding checks.")
+except Exception as e:
+    print(f"[ComfyUI-UtilsCollection] Failed to monkey-patch Qwen3VLTokenizer core tokenizer: {e}")
+
 from .encoder_nodes import *
 from .image_nodes import *
 from .preset_nodes import *
@@ -69,6 +120,7 @@ class SamplingUtils(ComfyExtension):
             UC_ExtractBoundingBox,
             UC_Krea2LayerProbe,
             UC_Krea2LayerAblator,
+            UC_Krea2InputEmbeds,
             UC_EncoderNodesGuide,
             AdjustedResolutionParameters,
             ResolutionSelectorExtended,
