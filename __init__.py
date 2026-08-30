04334ef37de96d39ce5ad3ea8cd9bdef1e9e416f
silveroxides <ishimarukaito@gmail.com>
2026-08-30 03:39:59 +0200

    Revert "feat: add ComfyUI restart menu command"
    This reverts commit af8e763d09d3a534bf025add6eaa0d445908b841.

--- __init__.py
+++ __init__.py
@@ -22,7 +22,6 @@ from comfy_api.latest import ComfyExtension, io
 from .node_replacements import register_replacements
 from .node_metadata import enrich_node_list
 from .scheduler_helpers import register_scheduler_handlers
-from .server_restart import register_restart_route
 
 class SamplingUtils(ComfyExtension):
     @override
@@ -239,7 +238,6 @@ class SamplingUtils(ComfyExtension):
     @override
     async def on_load(self) -> None:
         """Called by ComfyUI backend on startup to initialize resources and register API extensions."""
-        register_restart_route()
         register_scheduler_handlers()
         await register_replacements()
 
